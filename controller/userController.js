c4c9ecc5d826fef6fed037bdb5ace5d91ab83c06
abfiker <new94abemelek@gmail.com>
2024-10-04 14:23:43 +0300

    adding errorhandling and asynchandler middlerwares

--- controller/userController.js
+++ controller/userController.js
@@ -1,8 +1,9 @@
-const User = require('../model/userModel')
-const authUser = async(req,res)=>{
-    res.send('auth user')
-}
-const registerUser = async(req,res)=>{
-    res.send('register user')
-}
-module.exports = {authUser, registerUser}
+const User = require('../model/userModel');
+const asyncHandler = require('../middlerware/asyncHandler');
+const authUser = asyncHandler(async (req, res) => {
+  res.send('auth user');
+});
+const registerUser = asyncHandler(async (req, res) => {
+  res.send('register user');
+});
+module.exports = { authUser, registerUser };
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
+const {errorHandler,notfound} = require('./middlerware/errorHandler')
 dotenv.config();
 const connectDb = require('./config/connectDb');
 const app = express();
@@ -13,6 +14,8 @@ app.get('/', (req, res) => {
 });
 app.use('/api/users', userRoutes);
 const port = process.env.PORT || 8088;
+app.use(notfound)
+app.use(errorHandler)
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
