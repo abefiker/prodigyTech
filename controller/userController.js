98e3c95ec73629acc67b4fd2feacdb9a202f4499
abfiker <new94abemelek@gmail.com>
2024-10-04 16:30:39 +0300

    registering user is added successfully

--- controller/userController.js
+++ controller/userController.js
@@ -5,19 +5,13 @@ const generateToken = require('../utils/generateToken');
 const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
 
-  // Check for missing email or password
-  if (!email || !password) {
-    res.status(400);
-    throw new Error('Please provide both email and password');
-  }
-
-  console.log(email, password); // Debugging log for input
+  // Debugging log for input
   const user = await User.findOne({ email });
 
   console.log(user.email, ' user found'); // Debugging log when user is found
   if (user && (await user.comparePassword(password))) {
-    generateToken(user._id,res)
-    
+    generateToken(user._id, res);
+
     res.status(200).json({
       _id: user._id,
       name: user.name,
@@ -25,13 +19,30 @@ const authUser = asyncHandler(async (req, res) => {
       isAdmin: user.isAdmin,
     });
   } else {
-    // If password is incorrect
-    console.log('Password mismatch');
     res.status(401);
     throw new Error('Invalid email or password');
   }
 });
 const registerUser = asyncHandler(async (req, res) => {
-  res.send('register user');
+  const { name, email, password } = req.body;
+  const existingUser = await User.findOne({ email });
+  if (existingUser) {
+    res.status(400);
+    throw new Error('User already exists');
+  }
+
+  const user = await User.create({ name, email, password });
+  if (user) {
+    generateToken(user._id, res);
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(400);
+    throw new Error('Invalid user data');
+  }
 });
 module.exports = { authUser, registerUser };
